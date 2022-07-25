112c1dad49d7e6804787a8fdfef3ee7960d3251b
SrGesus <108523575+SrGesus@users.noreply.github.com>
2022-07-25 17:07:53 +0100

    Refactored and added second paddle

--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,183 @@
-use minifb::*;
+#![allow(dead_code)]
+use minifb::{Window, MouseMode, WindowOptions};
 use std::time::Instant;
 use std::cmp::{max, min};
 const WHITE : u32 = 16777215;
 const BLACK : u32 = 0;
 
-//window sizes
-const W_WIDTH : usize = 1000;
-const W_HEIGHT : usize = 600;
-//paddle size
-const P_WIDTH : usize = 10;
-const P_HEIGHT : usize = 80;
-//Ball radius (tho it's a square')
-const B_SIZE : usize = 5;
+struct Options {
+    w_width : usize,
+    w_height : usize,
+    p_width : usize,
+    p_height : usize,
+    b_size : usize,
+}
 
+struct Paddle {
+    x: f32,
+    y: f32,
+    vy: f32,
+    time: std::time::Instant
+}
+
+struct Ball {
+    x: f32,
+    y: f32,
+    vx: f32,
+    vy: f32,
+    time: std::time::Instant
+}
 
 fn main() {
-    
-    let mut paddle_pos = (P_HEIGHT, W_HEIGHT/2);
-    
-    let mut ball_pos: (f32, f32) = ((W_WIDTH/2) as f32, (W_HEIGHT/2) as f32);
-    let mut ball_speed = (-0.001, 0.001, Instant::now());
 
-    let mut window = Window::new("Pong!", W_WIDTH, W_HEIGHT, WindowOptions::default())
-        .expect("minifb was unable to create a new window.");
+    let options = Options {
+        //window sizes
+        w_width : 1000,
+        w_height : 600,
+
+        //paddle size
+        p_width : 10,
+        p_height : 80,
+
+        //ball radius
+        b_size : 5,
+    };
+
+    let mut paddle1 = Paddle {
+        x: (options.p_width*8) as f32,
+        y: (options.p_height/2) as f32,
+        vy: 0.0,
+        time: Instant::now()
+    };
+
+    let mut paddle2 = Paddle {
+        x: (options.w_width - options.p_width*8) as f32,
+        y: (options.w_height/2) as f32,
+        vy: 0.0,
+        time: Instant::now(),
+    };
+
+    let mut ball = Ball {
+        x: (options.w_width/2) as f32,
+        y: (options.w_height/2) as f32,
+        vx: -0.7,
+        vy: 0.4,
+        time: Instant::now()
+    };
+
+    let mut window = Window::new("Pong!", options.w_width, options.w_height, WindowOptions::default())
+        .expect("Minifb was unable to create a new window.");
+            
+    let mut buffer: Vec<u32> = vec![BLACK; options.w_width*options.w_height];
+
+    let mut fps = (0, Instant::now());
 
     loop {
-        //cleans the window
-        let mut buffer: Vec<u32> = vec![BLACK; W_WIDTH * W_HEIGHT];
+        if fps.1.elapsed().as_millis() > 1000 {
+            println!("Fps: {}", fps.0);
+            fps.0 = 0;
+            fps.1 = Instant::now();
+        }
 
-        draw_paddle(&mut buffer, &paddle_pos);
-        draw_ball(&mut buffer, &ball_pos);
+        draw_paddle(&mut buffer, &paddle1, &options, WHITE);
+        draw_paddle(&mut buffer, &paddle2, &options, WHITE);
+        draw_ball(&mut buffer, &ball, &options, WHITE);
 
-        window.update_with_buffer(&buffer, W_WIDTH, W_HEIGHT)
-            .expect("minifb was unable to update the window.");
-        
+        window.update_with_buffer(&buffer, options.w_width, options.w_height)
+            .expect("Minifb was unable to update the window.");
+
+        //erase the paddles and ball from the buffer
+        draw_paddle(&mut buffer, &paddle1, &options, BLACK);
+        draw_paddle(&mut buffer, &paddle2, &options, BLACK);
+        draw_ball(&mut buffer, &ball, &options, BLACK);
+
         //turn the y of the paddle into the y of the mouse
-        window.get_mouse_pos(MouseMode::Pass).map(|mouse| paddle_pos.1 = mouse.1 as usize);
+        window.get_mouse_pos(MouseMode::Pass).map(|mouse| paddle1.y = mouse.1 as f32);
 
-        //force bounds to prevent the paddle touching the outside of the screen
-        paddle_pos.1 = min(max(paddle_pos.1, P_HEIGHT), W_HEIGHT-P_HEIGHT);
-        
-        update_ball(&mut ball_pos, &mut ball_speed);
-
-    }
-}
-
-fn update_ball (pos: &mut (f32, f32), speed: &mut (f32, f32, std::time::Instant)) {
-    let time = speed.2.elapsed().as_millis();   //time since last update in milliseconds
-    pos.0 = pos.0 + (speed.0 * time as f32);
-    pos.1 = pos.1 + (speed.1 * time as f32);
+        //clamp the position of the paddle to the window
+        paddle1.y = min(max(paddle1.y as usize, options.p_height/2), options.w_height - options.p_height/2) as f32;
     
-    if pos.0 < P_HEIGHT as f32 {
-        pos.0 = P_HEIGHT as f32;
-        speed.0 = -speed.0;
-    } else if pos.0 > (W_WIDTH-P_HEIGHT) as f32 {
-        pos.0 = (W_WIDTH-P_HEIGHT) as f32;
-        speed.0 = -speed.0
+        update_ball(&mut ball, &paddle1, &paddle2, &options);
+        ball_paddle(&mut ball, &paddle1, &options);
+        ball_paddle(&mut ball, &paddle2, &options);
+        fps.0 += 1;
     }
-     if pos.1 < P_HEIGHT as f32 {
-        pos.1 = P_HEIGHT as f32;
-        speed.1 = -speed.1;
-    } else if pos.1 > (W_HEIGHT-P_HEIGHT) as f32 {
-        pos.1 = (W_HEIGHT-P_HEIGHT) as f32;
-        speed.1 = -speed.1
-    }   
 }
 
-fn draw_paddle (buffer: &mut Vec<u32>, pos: &(usize, usize)) {
-    let xbound = P_WIDTH/2;
-    let ybound = P_HEIGHT/2;
+fn update_ball(ball: &mut Ball, paddle1: &Paddle, paddle2: &Paddle, options: &Options) {
+    let time = ball.time.elapsed().as_millis() as f32;
+    ball.x += ball.vx * time;
+    ball.y += ball.vy * time;
+    
+    ball.time = Instant::now(); //refresh time
+    
+    let top_bound = options.b_size as f32;
+    let bot_bound = (options.w_height-options.b_size) as f32;
+    let left_bound = options.b_size as f32;
+    let right_bound = (options.w_width-options.b_size) as f32;
 
-    for i in 1..P_HEIGHT {
-        for j in 1..P_WIDTH {
-            buffer[W_WIDTH*(i + pos.1 as usize - ybound) + j + pos.0 as usize - xbound] = WHITE;
+    //reflect the ball if it hits a wall
+    if ball.x < left_bound {
+        ball.x = left_bound;
+        ball.vx = -ball.vx;
+    } else if ball.x > right_bound {
+        ball.x = right_bound;
+        ball.vx = -ball.vx;
+    }
+
+    if ball.y < top_bound {
+        ball.y = top_bound;
+        ball.vy = -ball.vy;
+    } else if ball.y > bot_bound {
+        ball.y = bot_bound;
+        ball.vy = -ball.vy;
+    }
+    
+}
+
+//ball paddle collision
+fn ball_paddle (ball: &mut Ball, paddle: &Paddle, options: &Options) {
+    let xdistance = ball.x - paddle.x;
+    let ydistance = ball.y - paddle.y;
+    let xbound = (options.p_width/2 + options.b_size) as f32;
+    let ybound = (options.p_height/2 + options.b_size*2) as f32;
+
+    if -xbound < xdistance && xdistance < xbound {
+        if -ybound < ydistance && ydistance < ybound {
+            ball.vx = -ball.vx;
+
+            if ball.vx < 0.0 {
+                ball.x = paddle.x - xbound;
+            } else {
+                ball.x = paddle.x + xbound;
+            }
         }
     }
 }
 
-fn draw_ball (buffer: &mut Vec<u32>, pos: &(f32, f32)) {
-    let diameter = B_SIZE * 2;
+fn draw_paddle (buffer: &mut Vec<u32>, paddle: &Paddle, options: &Options, color: u32) {
+    let xbound = options.p_width/2;
+    let ybound = options.p_height/2;
+
+    for i in 1..options.p_height {
+        for j in 1..options.p_width {
+            let x = j + paddle.x as usize - xbound;
+            let y = i + paddle.y as usize - ybound;
+
+            buffer[options.w_width * y + x] = color;
+        }
+    }
+}
+
+fn draw_ball (buffer: &mut Vec<u32>, ball: &Ball, options: &Options, color: u32) {
+    let diameter = options.b_size * 2;
 
     for i in 1..diameter {
         for j in 1..diameter {
-            buffer[W_WIDTH*(i + pos.1 as usize - B_SIZE) + j + pos.0 as usize - B_SIZE] = WHITE;
+            let x = j + ball.x as usize - options.b_size;
+            let y = i + ball.y as usize - options.b_size;
+
+            buffer[options.w_width * y + x] = color;
         }
     }
 }
